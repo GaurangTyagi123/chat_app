4de946721c9f251e67c5c8b1533a3320a4298f1f
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2025-02-22 20:27:30 +0530

    Deploying

--- resources/js/components/UserList.jsx
+++ resources/js/components/UserList.jsx
@@ -3,15 +3,15 @@ import React, { useState} from 'react';
 
 function UserList() {
     const [users] = useState(data.users);
-    const root = `${document.location.href.split("8000")[0]}8000`;
+    console.log(url)
     return (
         <ul className='chat__list'>
             {users.map(user => {
                 if(user.id !== data.user_id)
                     return <li className="chat__list__item" key={user.id} >
-                        <a href={`${root}/show/${user.id}`} className='chat__list__link' style={{textDecoration: "none"}} data-online="false" data-id={user.id}>
+                        <a href={`${url}/show/${user.id}`} className='chat__list__link' style={{textDecoration: "none"}} data-online="false" data-id={user.id}>
                         <div className="chat__details">
-                            <img src={`${root}/profile_pic.jpeg`} alt="profile pic" className='chat__details__img'/>
+                            <img src={`${url}/profile_pic.jpeg`} alt="gaurang tyagi" className='chat__details__img'/>
                             <span className='chat__details__name'>{user.name}</span>
                         </div>
                         </a>
