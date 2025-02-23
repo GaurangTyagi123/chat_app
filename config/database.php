92087d2320b0df6727f6cfffb89d9ab7e9174f47
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2025-02-24 00:28:24 +0530

    minor change

--- config/database.php
+++ config/database.php
@@ -44,8 +44,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL','mysql://root:JaqrSsZJurEoFXgzgiSXZekBFWQDCjVC@mysql.railway.internal:3306/railway'),
-            'host' => env('DB_HOST', 'mysql://root:JaqrSsZJurEoFXgzgiSXZekBFWQDCjVC@mysql.railway.internal:3306'),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
